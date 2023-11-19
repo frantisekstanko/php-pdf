cf65b07eea42f0defc3b0f90f9a6ae0fe8635de0
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-11-20 00:50:14 +0100

    specify array type

--- src/Fpdf/Fpdf.php
+++ src/Fpdf/Fpdf.php
@@ -88,7 +88,30 @@ final class Fpdf
     private string $currentFontStyle;
     private bool $isUnderline;
 
-    /** @var array<mixed> */
+    /** @var array{
+     *   i: int,
+     *   type: string,
+     *   name: string,
+     *   desc: array{
+     *      Ascent: int,
+     *      Descent: int,
+     *      CapHeight: int,
+     *      Flags: int,
+     *      FontBBox: string,
+     *      ItalicAngle: int,
+     *      StemV: int,
+     *      MissingWidth: int,
+     *   },
+     *   up: int,
+     *   ut: int,
+     *   ttffile: string,
+     *   fontkey: string,
+     *   subset: array<int, int>,
+     *   unifilename: string,
+     *   cw: array<string>,
+     *   MissingWidth?: int,
+     * }
+     */
     private array $currentFont;
     private float $currentFontSizeInPoints;
     private float $currentFontSize;
