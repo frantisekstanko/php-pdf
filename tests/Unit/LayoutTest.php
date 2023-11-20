1f2c30022c00dd90f78a3ce1d696867a923f6d9c
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-11-20 21:49:27 +0100

    add layout test

---
+++ tests/Unit/LayoutTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stanko\Fpdf\Tests\Unit;
+
+use DateTimeImmutable;
+use Stanko\Fpdf\Fpdf;
+use Stanko\Fpdf\Orientation;
+use Stanko\Fpdf\Tests\PdfTestCase;
+use Stanko\Fpdf\Units;
+
+final class LayoutTest extends PdfTestCase
+{
+    /**
+     * @testWith
+     * ["single", "ba24061dfff56446becf65ad0c3aefa22fde1f78"]
+     * ["continuous", "b11f1d8173a7ace608fc74d1df26f85414aa20ac"]
+     * ["two", "ff29e4e9d9d46a8dd6568a894b5066d6aaa9f54a"]
+     * ["default", "d22a77735a2152f2aa795ceb450a42bfdc34e470"]
+     */
+    public function testLayout(
+        string $layout,
+        string $expectedHash,
+    ): void {
+        $pdf = new Fpdf(
+            Orientation::PORTRAIT,
+            Units::MILLIMETERS,
+            'a4',
+        );
+        $pdf->setCreatedAt(new DateTimeImmutable('1999-12-26'));
+
+        $pdf->setDisplayMode('default', $layout);
+
+        $renderedPdf = $pdf->Output('S');
+
+        self::assertEquals($expectedHash, sha1($renderedPdf));
+    }
+}
