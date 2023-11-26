7e5431eef469645a72e2f42bd280a01cd49aa4c0
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-11-26 18:26:05 +0100

    improve the test
    page orientation should change in the same document

--- tests/Unit/PageOrientationTest.php
+++ tests/Unit/PageOrientationTest.php
@@ -5,43 +5,32 @@ declare(strict_types=1);
 namespace Stanko\Pdf\Tests\Unit;
 
 use DateTimeImmutable;
-use RuntimeException;
 use Stanko\Pdf\PageOrientation;
 use Stanko\Pdf\Pdf;
 use Stanko\Pdf\Tests\PdfTestCase;
 
 final class PageOrientationTest extends PdfTestCase
 {
-    /**
-     * @testWith
-     * ["portrait", "9a22fc8f732cc93942145f648d8aca238d9c4740"]
-     * ["landscape", "064aafe5ea0d278179924bc5921fcfc31a1b8743"]
-     */
-    public function testOrientation(
-        string $orientation,
-        string $expectedHash,
-    ): void {
-        $pdf = (new Pdf())->createdAt(new DateTimeImmutable('2023-12-26'))
-            ->withPageOrientation(
-                $this->pageOrientationFromString($orientation)
-            )
-        ;
+    public function testOrientation(): void
+    {
+        $expectedHash = '271a10c731cbb927c4fdf729a85430b63ece35c4';
 
-        $pdf->addPage();
-        $pdf->addPage();
-        $pdf->addPage();
+        $pdf = (new Pdf())->createdAt(new DateTimeImmutable('2023-12-26'));
+
+        $pdf->addPage(PageOrientation::PORTRAIT);
+        $pdf->addPage(PageOrientation::PORTRAIT);
+        $pdf->addPage(PageOrientation::PORTRAIT);
+
+        $pdf->addPage(PageOrientation::LANDSCAPE);
+        $pdf->addPage(PageOrientation::LANDSCAPE);
+        $pdf->addPage(PageOrientation::LANDSCAPE);
+
+        $pdf->addPage(PageOrientation::PORTRAIT);
+        $pdf->addPage(PageOrientation::PORTRAIT);
+        $pdf->addPage(PageOrientation::PORTRAIT);
 
         $renderedPdf = $pdf->toString();
 
         self::assertEquals($expectedHash, sha1($renderedPdf));
-    }
-
-    private function pageOrientationFromString(string $orientation): PageOrientation
-    {
-        return match ($orientation) {
-            'portrait' => PageOrientation::PORTRAIT,
-            'landscape' => PageOrientation::LANDSCAPE,
-            default => throw new RuntimeException(),
-        };
     }
 }
