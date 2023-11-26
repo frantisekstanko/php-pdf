e569b0e416482237065c60352c8cdfd668ec5bbb
Frantisek Stanko <frantisek.stanko@protonmail.com>
2023-11-26 19:16:39 +0100

    improve the test
    page size should change in the same document

--- tests/Unit/PageSizeTest.php
+++ tests/Unit/PageSizeTest.php
@@ -5,56 +5,41 @@ declare(strict_types=1);
 namespace Stanko\Pdf\Tests\Unit;
 
 use DateTimeImmutable;
-use RuntimeException;
 use Stanko\Pdf\PageSize;
 use Stanko\Pdf\Pdf;
+use Stanko\Pdf\RectangleStyle;
 use Stanko\Pdf\Tests\PdfTestCase;
 
 final class PageSizeTest extends PdfTestCase
 {
-    /**
-     * @testWith
-     * ["a3", "e455315deb50df5f7b6c6521ea50ccff03873756"]
-     * ["a4", "d22a77735a2152f2aa795ceb450a42bfdc34e470"]
-     * ["a5", "6a1b003c662a24e7f9d5a6cdc989e34d0ba6c0c8"]
-     * ["letter", "967923230e4521d5bcd76554eefaa85f35ebebb0"]
-     * ["legal", "324ca2db375d842c9ded265efe86c97a9f0d4fc8"]
-     */
-    public function testPageSizes(
-        string $pageSize,
-        string $expectedHash,
-    ): void {
-        $pdf = (new Pdf())->withPageSize($this->pageSizeFromString($pageSize))
-            ->createdAt(new DateTimeImmutable('1999-12-26'))
-        ;
+    public function testPageSizes(): void
+    {
+        $expectedHash = '6f641ea8c399065bf0efb776765d79ab86966b53';
+
+        $pdf = (new Pdf())->createdAt(
+            new DateTimeImmutable('1999-12-26')
+        );
+
+        $pdf->addPage(null, PageSize::a3());
+        $pdf->drawRectangle(10, 10, 100, 100, RectangleStyle::FILLED);
+
+        $pdf->addPage(null, PageSize::a4());
+        $pdf->drawRectangle(10, 10, 100, 100, RectangleStyle::FILLED);
+
+        $pdf->addPage(null, PageSize::a5());
+        $pdf->drawRectangle(10, 10, 100, 100, RectangleStyle::FILLED);
+
+        $pdf->addPage(null, PageSize::letter());
+        $pdf->drawRectangle(10, 10, 100, 100, RectangleStyle::FILLED);
+
+        $pdf->addPage(null, PageSize::legal());
+        $pdf->drawRectangle(10, 10, 100, 100, RectangleStyle::FILLED);
+
+        $pdf->addPage(null, PageSize::custom(400, 400));
+        $pdf->drawRectangle(10, 10, 100, 100, RectangleStyle::FILLED);
 
         $renderedPdf = $pdf->toString();
 
         self::assertEquals($expectedHash, sha1($renderedPdf));
-    }
-
-    private function pageSizeFromString(string $pageSize): PageSize
-    {
-        if ($pageSize === 'a3') {
-            return PageSize::a3();
-        }
-
-        if ($pageSize === 'a4') {
-            return PageSize::a4();
-        }
-
-        if ($pageSize === 'a5') {
-            return PageSize::a5();
-        }
-
-        if ($pageSize === 'letter') {
-            return PageSize::letter();
-        }
-
-        if ($pageSize === 'legal') {
-            return PageSize::legal();
-        }
-
-        throw new RuntimeException();
     }
 }
